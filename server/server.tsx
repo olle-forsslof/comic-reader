e506ad987de6ba6b18a09d94a043b66f2d752305
iths-olle-forsslof <54362382+iths-olle-forsslof@users.noreply.github.com>
2020-09-09 23:52:53 +0200

    fixed get online-comics route

--- server/server.tsx
+++ server/server.tsx
@@ -8,47 +8,93 @@ app.use(express.static(__dirname + '/../build'))
 
 // Sends back the folder name as title-continuos-title-name. The name sent in should be title of comic with - instead of spaces.
 // E.g dark-angels-of-darkness
-app.get('/comics/:title', async (req, res) => {
-    
-    const comic = req.params.title
-    console.log(comic);
-    
-    // try {
-        
-    //     // const comicFolder = await fs.readdirSync(__dirname + '/../public/comics', {withFileTypes: true})
-    //     // .filter(dirent => dirent.isDirectory())
-    //     // .map(dirent => dirent.name)
-    //     // .map(name => {
-    //     //     console.log(name)
-    //     //     return name.replace(' ', '-').toLowerCase()
-    //     // })
-    //     // .filter(title => title === reqName)
-    //     // res.send(comicFolder)
-    //     await fs.readdir(__dirname + `/../public/comics/${reqName}`, (err, files) => {
-    //         if (err) console.log(err);
-    //         let newArr = files.forEach(file => {
-    //             console.log('files: ', file);
-    //             return file
-    //         })
-    //         console.log('newArr:', newArr);
-            
-    //         res.send(newArr)
-            
-    //     })
+app.get('/online-comics/:title', async (req, res) => {
+  
+  const comic = req.params.title
+  let data = await getFileNames()
+  console.log(data);
+  
+  res.send(data)
 
-    // } catch(err) {
-    //     console.error({error: err})
-    // }
+  function getFileNames(){
+  return new Promise((resolve, reject) => {
+    const path = __dirname + `/../public/comics/${comic}`
+
+    // goes into the title folder which contains 2 folders large & small
+    fs.readdir(path, async (err, folders) => {
+      if (err) reject(err);
+
+      let promiseArray = []
+
+      //for each folder, create a new promise that resolves an object containing the names of all the files in that folder.
+      //save the promise in the promiseArray and then await all of them to be resolved before resolving the 
+      //main Promise.
+      folders.forEach(folder => {
+        promiseArray.push(
+          new Promise((resolve, reject) => {
+            fs.readdir(path + '/' + folder + '/', (err, pages) => {
+              if (err) reject(err);
+              
+              resolve({
+                [folder]: pages
+              })
+            })
+          })
+        )
+      })
+      let results = await Promise.all(promiseArray)
+      
+      resolve(results)
+
+    })
+  }) 
+
+
+  // try {
+  //     let images = {}
+  //     await fs.readdir(__dirname + `/../public/comics/${comic}`, (err, files) => {
+  //         if (err) console.log(err);
+          
+  //         files.map(file => {
+  //             const name = file.toString()
+  //             const pair = {name: []}
+
+  //             fs.readdir(__dirname +`/../public/comics/${comic}` + `/${file}`, (err, pages) => {
+  //                 pair.name = pages
+  //                 images = {...images, ...pair}
+                  
+                  
+  //                 console.log('images object = : ', images); // large & small
+  //             } )
+  //         })
+          // console.log('newArr:', newArr);
+      
+  //         res.send(newArr)
+      
+  //     // const comicFolder = await fs.readdirSync(__dirname + '/../public/comics', {withFileTypes: true})
+  //     // .filter(dirent => dirent.isDirectory())
+  //     // .map(dirent => dirent.name)
+  //     // .map(name => {
+  //     //     console.log(name)
+  //     //     return name.replace(' ', '-').toLowerCase()
+  //     // })
+  //     // .filter(title => title === reqName)
+  //     // res.send(comicFolder)
+  //     
+          
+  // })
+
+  }
 })
 
 app.get('/api/test', (req, res) => {
-    console.log('test is working');
-    
+  console.log('test is working');
+  
 })
 //TODO: make a call that sneds back all the image file names in an array. Needs to take in device width as parameter
 
 const serverPort = process.env.PORT || 8080; 
 
 app.listen(serverPort, () => {
-    console.log(`Server is running on port ${serverPort} `);
+  console.log(`Server is running on port ${serverPort} `);
 })
